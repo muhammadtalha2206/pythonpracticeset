5332f6173a21987e462a4d2b8d4b2a7016af7ce9
muhammadtalha2206 <94757076+muhammadtalha2206@users.noreply.github.com>
2022-12-20 01:25:05 -0800

    Add files via upload

---
+++ 2_ch.py
@@ -0,0 +1,21 @@
+
+
+# letter='''Dear, <|NAME|>\t
+# are you select in net sole company.
+# still now that they have watch.
+# Are you selected!
+# date: <|DATE|>
+# '''
+# name=input("Enter your name\n")
+# date=input("Enter date\n")
+# letter=letter.replace("<|NAME|>", name)
+# letter=letter.replace("<|DATE|>", date)
+# print(letter)
+
+# d="computer is  a dump machine.with out softwere is not  work."
+# print(d.replace("  "," "))
+
+# letter="Dear harry,\n\tThis python course is nice.\nThanks!"
+# print(letter)
+
+
